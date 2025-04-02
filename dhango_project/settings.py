45fe79ae1cd7eecd3d5eea6686a8c3e85f02e204
Kameron <ksmith46@my.waketech.edu>
2025-04-01 23:18:06 -0400

    newspaper app

--- dhango_project/settings.py
+++ dhango_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     #Local
     "accounts",
     "pages",
+    "articles",
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,5 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+TIME_ZONE = "America/New_York"
