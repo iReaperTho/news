de94af002364b68d2180266cb5d4ba673a60421d
Kameron <ksmith46@my.waketech.edu>
2025-04-08 22:05:20 -0400

    permissions and autherizations

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView, DetailView  # new
-from django.views.generic.edit import UpdateView, DeleteView  # new
-from django.urls import reverse_lazy  # new
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView 
+from django.urls import reverse_lazy
 from .models import Article
 from django.views.generic.edit import (
   UpdateView, DeleteView, CreateView
@@ -8,31 +12,43 @@ from django.views.generic.edit import (
 
 # Create your views here.
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
+    fields = ("title", "body")
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class ArticleDetailView(DetailView):  # new
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "article_detail.html"
 
 
-class ArticleUpdateView(UpdateView):  # new
+class ArticleUpdateView( LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = (
         "title",
         "body",
     )
     template_name = "article_edit.html"
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-
-class ArticleDeleteView(DeleteView):  # new
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
     
-class ArticleCreateView(CreateView):  # new
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+    
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_new.html"
     fields = (
@@ -40,3 +56,4 @@ class ArticleCreateView(CreateView):  # new
         "body",
         "author",
     )
+    
